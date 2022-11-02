e1b23b7e82a7886d07c92ce003715b134bdd4cb7
Стивън Лилов <SSLilov18@codingburgas.bg>
2022-11-02 15:02:22 +0200

    home

---
+++ src/main/java/com/example/stiv04/Stiv04Application.java
@@ -0,0 +1,13 @@
+package com.example.stiv04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stiv04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Stiv04Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stiv04/Stiv04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stiv04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stiv04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
