bf5b0dc4a947a168517cc00967b1bf10c16c2958
Стивън Лилов <SSLilov18@codingburgas.bg>
2022-11-02 15:05:48 +0200

    age info

--- src/main/java/com/example/stiv04/age.java
+++ src/main/java/com/example/stiv04/age.java
@@ -1,4 +1,11 @@
 package com.example.stiv04;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class age {
+
+    @GetMapping("/age")
+    public String info(){
+        return "18 years old.";
+    }
 }
